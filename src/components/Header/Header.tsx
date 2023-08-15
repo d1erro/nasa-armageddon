31d0652bc3129e7022f57112c35eb44d20a5172e
Damir Gimazetdinov <d1erro7@gmail.com>
2023-08-15 20:15:56 +0300

    Edit title

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -10,7 +10,7 @@ const Header = () => {
             <header className={styles.header}>
                 <Link href="/" className={styles.headerLink}>
                     <h1 className={`${passionOne.className} ${styles.headerTitle}`}>Armageddon 2023</h1>
-                    <h4 className={styles.headerDescription}>ООО "Команда им. Б. Уиллиса".</h4>
+                    <h4 className={styles.headerDescription}>ООО Команда им. Б. Уиллиса</h4>
                     <h4 className={styles.headerDescription}>Взрываем астероиды с 1998 года.</h4>
                 </Link>
             </header>
