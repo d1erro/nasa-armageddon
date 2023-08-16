a3e1d1920da2e8461b4160d830ef6344e421511e
Damir Gimazetdinov <d1erro7@gmail.com>
2023-08-16 21:54:15 +0300

    Add force cache

--- src/api/get-asteroid-info.ts
+++ src/api/get-asteroid-info.ts
@@ -1,5 +1,8 @@
 export const getAsteroidInfo = async (id: number) => {
-        const response = await fetch(`https://api.nasa.gov/neo/rest/v1/neo/${id}?api_key=${process.env.NEXT_PUBLIC_NASA_API}`)
+        const response = await fetch(
+            `https://api.nasa.gov/neo/rest/v1/neo/${id}?api_key=${process.env.NEXT_PUBLIC_NASA_API}`,
+            {cache: 'force-cache'}
+        )
         const asteroid = await response.json();
         return asteroid;
 }
--- src/api/get-asteroid-list.ts
+++ src/api/get-asteroid-list.ts
@@ -1,6 +1,8 @@
 export const getAsteroidList = async (fetchDate: string) => {
         const response = await fetch(`https://api.nasa.gov/neo/rest/v1/feed?start_date=${fetchDate}&end_date=
-        ${fetchDate}&api_key=${process.env.NEXT_PUBLIC_NASA_API}`);
+        ${fetchDate}&api_key=${process.env.NEXT_PUBLIC_NASA_API}`,
+            {cache: 'force-cache'}
+        );
         const data = await response.json();
         const newAsteroids = data.near_earth_objects[fetchDate];
         return newAsteroids;
